031f1783154d08c8bf933a28c28687f85c28b6e1
frankbjj23 <frankbjj23@gmail.com>
2025-11-15 14:14:02 -0500

    created new camelCase and added return element

--- index.js
+++ index.js
@@ -12,7 +12,18 @@ const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
 
 //1. Write a function that returns a freelancer object with a randomly generated name, occupation, and rate according to the provided constants.
-function RandomFreelancer() {
-  const RandomElement = (array) =>
+function randomFreelancer() {
+  const randomElement = (array) =>
     array[Math.floor(Math.random() * array.length)];
+
+  const randomRate = () => {
+    const range = PRICE_RANGE.max - PRICE_RANGE.min;
+    return Math.floor(Math.random() * range) + PRICE_RANGE.min;
+  };
+  return {
+    name: randomElement(NAMES),
+    occupation: randomElement(OCCUPATIONS),
+    rate: randomRate(),
+  };
 }
+console.log(randomFreelancer());
